2c465bdd7c294e665ae2d9fa9e8e7ce16f39d37c
Brice Pieterse <pietersebrice@gmail.com>
2021-10-13 17:54:14 -0400

    Add get product method to inventory service; use it in product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { InventoryService } from '../services/inventory.service'
+import { Product } from '../models/product'
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
+
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  product: Product
+
+  constructor(private inventoryService: InventoryService) { }
 
   ngOnInit(): void {
+    this.inventoryService.getProduct(1).subscribe(product => {
+      if (product){
+        this.product = product
+      }
+    })
   }
 
 }
--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Product } from '../models/product'
 import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,16 @@ export class InventoryService {
 
   constructor(private http: HttpClient) { }
 
-  getInventory():Observable<Product[]> {
+  getInventory(): Observable<Product[]> {
     return this.http.get<Product[]>('../../assets/products.json')
+  }
+
+  getProduct(id: number): Observable<Product | undefined >  {
+    return this.http.get<Product[]>('../../assets/products.json').pipe(
+      map((products): Product | undefined => {
+        return products.find( product => product.id === id)
+      })
+    )
   }
 
 }
