58d919c3536d550fd0a9a5ce3e5289b6b7cf8c29
Brice Pieterse <pietersebrice@gmail.com>
2021-10-26 20:08:46 -0400

    Add toast when user adds to or removes from cart; Update README; Add product price to main views

--- README.md
+++ README.md
@@ -6,22 +6,16 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
 
-## Running unit tests
+## Project Description
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+The project is a simple storefront for popular tv shows. It was used to practice working with Angular to build a SPA. A few key features are:
 
-## Running end-to-end tests
+- Cart indicator that displays how many items are in the user's cart from any view in the application.
 
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+- Cart order form that includes custom validators.
 
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+- Dark theme 😍
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild  } from '@angular/core';
 import { InventoryService } from '../services/inventory.service'
 import { CartService } from '../services/cart.service'
 import { Product } from '../models/product'
@@ -13,8 +13,11 @@ import { Location } from '@angular/common'
 
 export class ProductComponent implements OnInit {
 
+  @ViewChild ('cartButton') cartButton : ElementRef
+
   cartCount: number
   product: Product
+  addedToCart: boolean = false
 
   constructor(private inventoryService: InventoryService, private cartService: CartService, private location: Location, private route: ActivatedRoute) { }
 
@@ -33,10 +36,18 @@ export class ProductComponent implements OnInit {
   }
 
   addToCart(): void {
-    this.cartService.addToCart(this.product)
-    this.cartCount = this.cartService.getCartProducts().length
+    if (!this.addedToCart){
+      this.cartService.addToCart(this.product)
+      this.cartCount = this.cartService.getCartProducts().length
+      this.addedToCart = true
+      this.cartButton.nativeElement.innerText = `Remove From Cart - ${this.product.price}`
+    }
+    else {
+      this.cartService.removeFromCart(this.product)
+      this.cartCount = this.cartCount - 1
+      this.addedToCart = false
+      this.cartButton.nativeElement.innerText = `Add To Cart - ${this.product.price}`
+    }
   }
-
-  
 
 }
